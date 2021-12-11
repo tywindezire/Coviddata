6db6c1df971bf894a06972c74def14783ebc8f84
tywindezire <81945539+tywindezire@users.noreply.github.com>
2021-12-11 09:40:59 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,12 @@
 from flask import Flask
 import os
 
+port = int(os.environ.get('PORT', 5000))
 app = Flask(__name__)
+
 
 @app.route("/")
 def home_view():
 		return "<h1>Welcome to Geeks for Geeks</h1>"
+
+app.run(host='0.0.0.0', port=port, debug=True)
